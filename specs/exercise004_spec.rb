00d0afabad5700ebcfc6d29d578de41b3e21f87b
Eric Rochester <err8n@virginia.edu>
2011-10-17 15:58:14 -0400

    Added line count check for exercise-004.rb.

--- specs/exercise004_spec.rb
+++ specs/exercise004_spec.rb
@@ -6,6 +6,10 @@ describe "exercise-004.rb" do
       @lines = output.lines
     end
 
+    it "should write one line for each token type in jabberwocky.txt" do
+      @lines.count.should == 90
+    end
+
     it "should not include empty words." do
       # Just use the same regex as last time, but use +, not *.
       @lines.each { |line| (/^'[a-z]+'\t\d+\n$/ =~ line).should == 0 }
