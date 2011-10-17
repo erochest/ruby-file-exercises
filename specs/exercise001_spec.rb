ee7a9dd3f04a7c298b7b62d566ff003db38c43dc
Eric Rochester <err8n@virginia.edu>
2011-10-17 15:13:16 -0400

    Refactored and improved tests for exercise-001.rb.

--- specs/exercise001_spec.rb
+++ specs/exercise001_spec.rb
@@ -1,11 +1,18 @@
 
 describe "exercise-001.rb" do
   describe "output" do
-    it "should write the words from jabberwocky.txt to the screen, one per line." do
+    before(:each) do
       output = `ruby exercise-001.rb`
-      lines = output.lines
-      lines.count.should == 167
-      lines.each { |line| (/\s/ =~ line.rstrip).should == nil }
+      @lines = output.lines
+    end
+
+    it "should write the words from jabberwocky.txt to the screen, one per line." do
+      @lines.count.should == 167
+      @lines.to_a[0, 6].join().should == "`Twas\nbrillig,\nand\nthe\nslithy\ntoves\n"
+    end
+
+    it "should remove whitespace from the words." do
+      @lines.each { |line| (/\s/ =~ line.rstrip).should == nil }
     end
   end
 end
